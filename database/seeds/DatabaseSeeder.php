59cdc16b9d9acb7bcabff5f9009bf9a855c28335
Scott Johnston <scott.v.johston@gmail.com>
2015-12-07 19:40:20 +1100

    all of the seeders uploaded

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(UsersTableSeeder::class);
-        //$this->call(EscapesTableSeeder::class);
-        //$this->call(HolidaysTableSeeder::class);
-        //$this->call(EscapeHolidayTableSeeder::class);
+        $this->call(EscapesTableSeeder::class);
+        $this->call(HolidaysTableSeeder::class);
+        $this->call(EscapeHolidayTableSeeder::class);
 
         Model::reguard();
     }
